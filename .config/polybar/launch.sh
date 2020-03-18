e32262268bff950832e830d53b17018dc6eefea8
Karl Hepworth <karl.hepworth@gmail.com>
2020-03-18 23:00:17 +1100

    initial commit
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+polybar top-bar -r &
