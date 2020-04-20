e638decae866e16c85c5d90ad52dfc77b5ce20a6
Karl Hepworth <karl.hepworth@gmail.com>
2020-04-20 22:46:18 +1000

    wowzers
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-# Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
-
-polybar top-bar -r &
-polybar coronavirus -r &
+polybar bar-top     -r  &
+polybar bar-bottom  -r  &
