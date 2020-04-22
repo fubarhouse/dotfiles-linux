1ef04259d1a9fb8d02ef8724aee9b4a77c0f8715
Karl Hepworth <karl.hepworth@gmail.com>
2020-04-22 22:33:36 +1000

    cleanup
    Signed-off-by: Karl Hepworth <karl.hepworth@gmail.com>

--- .config/i3/generate.sh
+++ .config/i3/generate.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 DIRECTORY="${HOME}/.config/i3"
-echo "" > "${DIRECTORY}/config"
 
-find "${DIRECTORY}/includes/"*.conf -type f | while IFS= read -r file; do
-  cat "${file}" >> "${DIRECTORY}/config"
-  echo "" >> "${DIRECTORY}/config"
+{
+  echo "# This file was generated from a series of inclusions."
+  echo "# Do not change this file directly as your changes will not persist."
+} > ${DIRECTORY}/config
+
+find "${DIRECTORY}/includes" -iname *.conf -type f | sort | while IFS= read -r file; do
+  echo $file
+  {
+    echo "";
+    cat "${file}";
+    echo "";
+  } >> ${DIRECTORY}/config;
 done
